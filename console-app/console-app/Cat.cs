7e54d10806d552f501dcbc0aa9bc529f1647f1ce
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-12-28 06:32:05 -0300

    Classes basics

---
+++ console-app/console-app/Cat.cs
@@ -0,0 +1,12 @@
+﻿namespace console_app
+{
+    public class Cat
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public bool IsAngry { get; private set; } = false;
+
+        public void CheckAngry() => IsAngry = Name == null || Name.Contains("a");
+    }
+}
--- console-app/console-app/Program.cs
+++ console-app/console-app/Program.cs
@@ -7,41 +7,17 @@ namespace console_app
     {
         public static void Main(string[] args)
         {
-            MeetAlien();
-            Console.WriteLine("----------------");
-            MeetAlien();
-            Console.WriteLine("----------------");
+            var cat = new Cat
+            {
+                Name = "Paco",
+                Age = 3
+            };
 
-            Console.WriteLine("Insert a number to square it");
-            var squaredNumber = Square(Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine($"Result is: {squaredNumber}");
-            Console.WriteLine("----------------");
+            Console.WriteLine($"Cat angry: {cat.IsAngry}");
+            
+            cat.CheckAngry();
+            Console.WriteLine($"Cat angry: {cat.IsAngry}");
 
-            Console.WriteLine("Insert a number");
-            var userNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(IsEven(userNumber) ? $"{userNumber} is even" : $"{userNumber} is not even");
-            Console.WriteLine("----------------");
-
-            const string someString = "This is a string word";
-            var splattedString = someString.Split(" ").ToList();
-
-            Console.WriteLine(someString);
-            Console.WriteLine("-------- Splitted word --------");
-            splattedString.ForEach(Console.WriteLine);
         }
-
-        private static void MeetAlien()
-        {
-            var random = new Random();
-            var name = $"X-{random.Next(1000, 5001)}";
-            var age = random.Next(70, 501);
-            Console.WriteLine($"Hi, I'm {name},");
-            Console.WriteLine($"I'm {age} years old.");
-            Console.WriteLine($"I'm an alien...");
-        }
-
-        private static double Square(double number) => Math.Pow(number, 2);
-
-        private static bool IsEven(double number) => number % 2 == 0;
     }
 }
