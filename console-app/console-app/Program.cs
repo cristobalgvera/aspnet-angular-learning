15f70fb579dec6c4755a543076bf6d227b7fe611
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-12-28 03:39:08 -0300

    C# console basics

--- console-app/console-app/Program.cs
+++ console-app/console-app/Program.cs
@@ -4,9 +4,21 @@ namespace console_app
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Skynet!";
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            const int someNumber = 3;
+
+            Console.WriteLine($"Hello World! Number: {someNumber}");
+            Console.WriteLine("What's your name?");
+
+            var name = Console.ReadLine();
+            
+            Console.WriteLine($"Hello {(name != "" ? name : "user")}");
+
+            Console.ReadKey();
         }
     }
 }
