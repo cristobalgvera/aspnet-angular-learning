0c233af096b842e8b731e83de853157896d0ad50
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-12-28 04:10:46 -0300

    Data types basics

--- console-app/console-app/Program.cs
+++ console-app/console-app/Program.cs
@@ -4,7 +4,7 @@ namespace console_app
 {
     class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Title = "Skynet!";
             Console.ForegroundColor = ConsoleColor.Red;
@@ -15,8 +15,22 @@ namespace console_app
             Console.WriteLine("What's your name?");
 
             var name = Console.ReadLine();
-            
+
             Console.WriteLine($"Hello {(name != "" ? name : "user")}");
+
+            Console.WriteLine("Write a number below :)");
+            var consoleNumber = 0.0;
+
+            try
+            {
+                consoleNumber = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            Console.WriteLine($"{consoleNumber} / 3 = {consoleNumber / 3}");
 
             Console.ReadKey();
         }
