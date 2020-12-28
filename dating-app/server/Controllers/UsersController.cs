0864225ac7caa047c666778abae79851ddc10cb6
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-12-28 08:08:22 -0300

    gitignore update

---
+++ dating-app/server/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Entities;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers() => await _context.Users.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id) => await _context.Users.FindAsync(id);
+    }
+}
---
+++ dating-app/server/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Entities;
+
+namespace server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
