e156db046a5f8d958f084f62404ae97d76c7cd58
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-12-28 04:29:55 -0300

    Conditionals basics

--- console-app/console-app/Program.cs
+++ console-app/console-app/Program.cs
@@ -7,7 +7,7 @@ namespace console_app
         public static void Main(string[] args)
         {
             Console.Title = "Skynet!";
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
 
             const int someNumber = 3;
 
@@ -16,7 +16,7 @@ namespace console_app
 
             var name = Console.ReadLine();
 
-            Console.WriteLine($"Hello {(name != "" ? name : "user")}");
+            Console.WriteLine($"Hello {(!string.IsNullOrEmpty(name) ? name : "user")}");
 
             Console.WriteLine("Write a number below :)");
             var consoleNumber = 0.0;
@@ -29,10 +29,57 @@ namespace console_app
             {
                 Console.WriteLine(e);
             }
-            
-            Console.WriteLine($"{consoleNumber} / 3 = {consoleNumber / 3}");
 
-            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            // Inline conditional
+            Console.WriteLine(consoleNumber >= 13
+                ? $"{consoleNumber} / 3 = {consoleNumber / 3}"
+                : $"{consoleNumber} * 3 = {consoleNumber * 3}");
+
+            // Multiline conditional
+            if (!string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name isn't empty, nice!");
+            }
+            else
+            {
+                Console.WriteLine("Name is empty, what a pity");
+            }
+
+            if (!string.IsNullOrEmpty(name) && consoleNumber > 20)
+            {
+                Console.WriteLine("Requirements passed");
+            }
+            else
+            {
+                Console.WriteLine("Requirements aren't passed");
+            }
+
+            Console.WriteLine("Write a number between 1 and 5: ");
+            consoleNumber = Convert.ToDouble(Console.ReadLine());
+
+            switch (consoleNumber)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                default:
+                    Console.WriteLine("Ups, wrong number");
+                    break;
+            }
         }
     }
 }
