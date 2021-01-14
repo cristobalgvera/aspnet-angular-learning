7f560ec4ecabcf33e82cb2522eca1e1aa1dd1371
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-14 20:21:52 -0300

    Basic types usage

--- console/console/Program.cs
+++ console/console/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace console
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string name = "I'm Cris";
+            var number = 3;
+
+            Console.WriteLine(name + number);
+
+            number = 5;
+
+            Console.WriteLine(name + number);
+
+            var date = DateTime.Now;
+            const bool civilState = true;
+
+            Console.WriteLine(!civilState);
+            Console.WriteLine(date);
+
+            const State state = State.Active;
+
+            Console.WriteLine(state);
+
+            var isMeantToBeNull = CanBeNull();
+
+            Debug.Assert(isMeantToBeNull != null, nameof(isMeantToBeNull) + " != null");
+            Console.WriteLine(isMeantToBeNull);
         }
+
+        private static bool? CanBeNull() => new Random().Next(100) > 50 ? (bool?) null : true;
+    }
+
+    internal enum State
+    {
+        Active,
+        Inactive
     }
 }
