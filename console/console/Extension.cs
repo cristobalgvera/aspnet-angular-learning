16fb62b30bd8bfcd11a4c3136ebd696294c9b2dd
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-15 02:51:14 -0300

    Delegate methods, Func and Action usages

--- console/console/Extension.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace console
-{
-    internal static class Extension
-    {
-        public static bool IsEven(this int number) => number % 2 == 0;
-    }
-}
--- console/console/Program.cs
+++ console/console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace console
 {
@@ -6,10 +7,42 @@ namespace console
     {
         public static void Main(string[] args)
         {
-            const int aNumber = 103;
+            Console.WriteLine(CalculateExecutionTime(SendMessage));
+            Console.WriteLine(CalculateExecutionTime(() => Thread.Sleep(500))); // Delegate can be an anonymous function
 
-            // Usage of extension method called "IsEven" for int objects
-            Console.WriteLine($"Number {aNumber} is {(aNumber.IsEven() ? "even" : "odd")}");
+            Func<int, int, bool> func = Equals; // To use a function that return a value as a variable
+            // var func = new Func<int, int, int>(Equals); -> A fancy way of doing above stuff
+
+            var result = func(5, 6);
+            Console.WriteLine(result);
+
+            Action<string> action = Greet; // To use a function that return VOID as a variable
+            // var action = new Action<string>(Greet); -> A fancy way of doing above stuff
+
+            action("Hello");
+
+            // An ugly way to make inline functions and store it in variables, but works well
+            var inLineAction = new Action<string>((string word) => Console.WriteLine($"Hello World! - {word}"));
+            var inLineFunc = new Func<int, int>((int number) => 4 + number);
+
+            inLineAction("YOUUU!");
+            Console.WriteLine(inLineFunc(5));
         }
+
+        private delegate void DelegateExample(); // Used to provide a method (delegate) to a function as parameter
+
+        private static void SendMessage() => Thread.Sleep(1000);
+
+        private static double CalculateExecutionTime(DelegateExample delegateExample)
+        {
+            var startingTime = DateTime.Now;
+            delegateExample();
+            var endingTime = DateTime.Now;
+            return (endingTime - startingTime).TotalMilliseconds;
+        }
+
+        private static bool Equals(int a, int b) => a == b;
+
+        private static void Greet(string message) => Console.WriteLine(message);
     }
 }
