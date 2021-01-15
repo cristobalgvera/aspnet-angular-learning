0fef767b6f40481f81dc5a3ae13e32f7aeb8b8fb
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-14 21:20:09 -0300

    If usage

--- console/console/Program.cs
+++ console/console/Program.cs
@@ -6,13 +6,19 @@ namespace console
     {
         public static void Main(string[] args)
         {
-            var myTuple = ("First", 10); 
+            Console.WriteLine("Enter your age");
+            var age = int.Parse(Console.ReadLine()); // Will fail if no input
 
-            Console.WriteLine(myTuple.Item1);
+            if (age >= 18)
+                Console.WriteLine("Your are older");
+            else if (age == 13)
+                Console.WriteLine("You have 13!!");
+            else
+                Console.WriteLine("You're too young");
 
-            var (name, age) = ("Eduardo", 29);
+            // Ternary operator way
 
-            Console.WriteLine($"I'm {name} and I have {age} years old");
+            Console.WriteLine(age >= 18 ? "Your are older" : "You're too young");
         }
     }
 }
