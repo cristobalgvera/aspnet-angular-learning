344f5775c6c3fed667c11b9127480dd4d8d36840
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-15 00:55:20 -0300

    Methods usage II

--- console/console/Program.cs
+++ console/console/Program.cs
@@ -13,6 +13,10 @@ namespace console
             Console.WriteLine(fullName);
 
             Console.WriteLine(person.Sum(4, 6));
+
+            // Usage of static methods
+            Console.WriteLine($"Sum of three numbers: {Math.Sum(4, 6, 7)}");
+            Console.WriteLine($"Subtraction of two numbers: {Math.Subtraction(4, 6)}");
         }
 
         private class Person
@@ -20,6 +24,17 @@ namespace console
             public string Concatenate(string name, string lastName) => $"{name} {lastName}";
 
             public int Sum(int a, int b) => a + b;
+        }
+
+        private class Math
+        {
+            // Overload example
+            public static int Sum(int a, int b) => a + b;
+
+            public static int Sum(int a, int b, int c) => a + b + c;
+
+            // Default parameters values example
+            public static int Subtraction(int a, int b, int c = 0) => a - b - c;
         }
     }
 }
