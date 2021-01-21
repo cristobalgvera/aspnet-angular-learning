d96eca45beffe5416b41ea0c7ec63b64db072f4e
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-21 10:57:32 -0300

    Third party libraries added and theme update

--- BulkyBook/BulkyBook/Startup.cs
+++ BulkyBook/BulkyBook/Startup.cs
@@ -28,7 +28,7 @@ namespace BulkyBook
 
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
