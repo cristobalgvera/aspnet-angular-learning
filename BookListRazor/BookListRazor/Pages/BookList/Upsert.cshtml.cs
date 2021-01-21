818ffd0395914b0c39772e24ae87390aa07788df
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-21 01:55:43 -0300

    🎉 BulkyBook project init

--- BookListRazor/BookListRazor/Pages/BookList/Upsert.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -5,28 +5,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazor.Pages.BookList
 {
-    public class Edit : PageModel
+    public class Upsert : PageModel
     {
         private readonly ApplicationDbContext _dbContext;
 
-        public Edit(ApplicationDbContext dbContext)
+        public Upsert(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
         [BindProperty] public Book Book { get; set; }
 
-        public async Task OnGet(int id) => Book = await _dbContext.Book.FindAsync(id);
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            // Create
+            if (id == null)
+            {
+                Book = new Book();
+                return Page();
+            }
+
+            // Update
+            Book = await _dbContext.Book.FindAsync(id);
+            return Book == null ? NotFound() : Page();
+        }
 
         public async Task<IActionResult> OnPost()
         {
             if (!ModelState.IsValid) return Page();
 
-            var dbBook = await _dbContext.Book.FindAsync(Book.Id);
-            dbBook.Name = Book.Name;
-            dbBook.Author = Book.Author;
-            dbBook.ISBN = Book.ISBN;
-
+            if (Book.Id == 0) await _dbContext.Book.AddAsync(Book);
+            else _dbContext.Book.Update(Book);
+            
             await _dbContext.SaveChangesAsync();
             return RedirectToPage("Index");
         }
