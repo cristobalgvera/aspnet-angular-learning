2781ade071bc20df2d113a3298b7120d0afea9b5
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-28 03:00:59 -0300

    Repository pattern semi implemented

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Update(Category category)
+        {
+            var categoryFromDb = _dbContext.Categories.FirstOrDefault(cat => cat.Id == category.Id);
+            if (categoryFromDb == null) return;
+
+            categoryFromDb.Name = category.Name;
+            _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ BulkyBook/BulkyBook.DataAccess/Repository/Repository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        protected Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public T Get(int id) => _dbSet.Find(id);
+
+        public IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+        )
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (filter != null) queryable = queryable.Where(filter);
+
+            if (includeProperties != null)
+                queryable = includeProperties
+                    .Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)
+                    .Aggregate(queryable, (current, includeProperty) =>
+                        current.Include(includeProperty)
+                    );
+
+            return orderBy?.Invoke(queryable).ToList() ?? queryable.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> queryable = _dbSet;
+
+            if (filter != null) queryable = queryable.Where(filter);
+
+            if (includeProperties != null)
+                queryable = includeProperties
+                    .Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)
+                    .Aggregate(queryable, (current, includeProperty) =>
+                        current.Include(includeProperty)
+                    );
+
+            return queryable.FirstOrDefault();
+        }
+
+        public void Add(T entity) => _dbSet.Add(entity);
+
+        public void Remove(int id)
+        {
+            var entity = _dbSet.Find(id);
+            Remove(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
