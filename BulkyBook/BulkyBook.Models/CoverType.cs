6681faef75efb44541011f6f0a3fa3511c5838a3
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-01-29 23:40:29 -0300

    Add Cover Type assignment half completed
    TODO:
    - Add CoverTypeRepository
    - Add ICoverTypeRepository
    - Add CoverType to UnitOfWork and IUnitOfWork

---
+++ BulkyBook/BulkyBook.Models/CoverType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class CoverType
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Cover Type Name")]
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+    }
+}
